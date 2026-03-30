6eb9fb54a590f5016fdd54fb5e01859193d1b2ff
Jairuthra  R <142900214+jairuthrar@users.noreply.github.com>
2026-03-30 12:35:22 +0100

    Update

--- LibraryManagement/src/main/java/com/example/LibraryManagement/Service/EmailService.java
+++ LibraryManagement/src/main/java/com/example/LibraryManagement/Service/EmailService.java
@@ -16,7 +16,7 @@ public class EmailService {
         message.setTo(to);
         message.setSubject(subject);
         message.setText(body);
-        message.setFrom("your-email@gmail.com");
+        message.setFrom("jairuthraramachandran@gmail.com");
 
         mailSender.send(message);
     }
