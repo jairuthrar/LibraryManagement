636347602d59658b6a7c26e5f1583a45aee654d0
Jairuthra  R <142900214+jairuthrar@users.noreply.github.com>
2026-03-30 12:36:16 +0100

    Add changes
    Added methods to find users by username and email.

--- LibraryManagement/src/main/java/com/example/LibraryManagement/UserRepository.java
+++ LibraryManagement/src/main/java/com/example/LibraryManagement/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
-    // Optional: find by username or email
+ 
     UserEntity findByUsername(String username);
     UserEntity findByEmail(String email);
 }
